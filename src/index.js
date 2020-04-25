0ec22abd34509c4e49529d618583a526f3745a50
Garrett <garrettgassensmith@gmail.com>
2020-04-25 18:18:52 -0500

    populated actions object, created ask function and connected functionality

--- src/index.js
+++ src/index.js
@@ -16,26 +16,244 @@ const menu = [
     name: "action",
     message: "What do you want to do?",
     choices: [
-        new inquirer.Separator(chalk.green("--- Add ----")),
-        "Add Department",
-        "Add Role",
-        "Add Employee",
-        new inquirer.Separator(chalk.cyan("--- View ----")),
-        "View Departments",
-        "View Roles",
-        "View Employees",
-        new inquirer.Separator(chalk.yellow("--- Update ----")),
-        "Update Employee Role",
-        new inquirer.Separator(chalk.red("--- Delete ----")),
-        "Delete Department",
-        "Delete Role",
-        "Delete Employee",
+      new inquirer.Separator(chalk.green("--- Add ----")),
+      "Add Department",
+      "Add Role",
+      "Add Employee",
+      new inquirer.Separator(chalk.cyan("--- View ----")),
+      "View Departments",
+      "View Roles",
+      "View Employees",
+      new inquirer.Separator(chalk.yellow("--- Update ----")),
+      "Update Employee Role",
+      new inquirer.Separator(chalk.red("--- Delete ----")),
+      "Delete Department",
+      "Delete Role",
+      "Delete Employee",
+      new inquirer.Separator(chalk.white("-------------")),
+      "EXIT",
     ],
+    filter: function (choice) {
+      switch (choice) {
+        case "Add Department":
+          return "addDept";
+        case "Add Role":
+          return "addRole";
+        case "Add Employee":
+          return "addEmployee";
+        case "View Departments":
+          return "viewDepts";
+        case "View Roles":
+          return "viewRoles";
+        case "View Employees":
+          return "viewEmployees";
+        case "Update Employee Role":
+          return "updateEmployeeRole";
+        case "Delete Department":
+          return "deleteDept";
+        case "Delete Role":
+          return "deleteRole";
+        case "Delete Employee":
+          return "deleteEmployee";
+        case "EXIT":
+          return "exit";
+      }
+    },
   },
 ];
 
 // Actions
-const actions = {};
+const actions = {
+  addDept: /* add */ function() {
+    console.log("--- Add a department ---");
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "dept_name",
+          message: "Enter Department name",
+        },
+      ])
+      .then((res) => {
+        departments.addDepartment(res.dept_name);
+        setTimeout(ask, 200);
+      });
+  },
+  addRole: function () {
+    inquirer
+      .prompt({
+        type: "input",
+        name: "artist",
+        message: "Enter an artist to search or nothing to get everything.",
+      })
+      .then((res) => {
+        if (res.artist == "") {
+          playlist.getAllPlaylists();
+        } else {
+          playlist.getPlaylistsByColumn("artist", res.artist);
+        }
+        setTimeout(ask, 200);
+      });
+  },
+  addEmployee: function () {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "id",
+          message: "Enter an id to update.",
+        },
+        {
+          type: "input",
+          name: "colval",
+          message: `Enter 'column|newvalue'`,
+        },
+      ])
+      .then((res) => {
+        if (res.id == "") {
+          console.log("You must enter an id");
+          ask();
+        } else {
+          let update = {};
+          update[res.colval.split("|")[0]] = res.colval.split("|")[1];
+          /*
+                    res.colval.split('|') == ['column', 'newvalue']
+                    update['column'] = 'newvalue'
+                  */
+          playlist.updatePlaylist(res.id, update);
+        }
+        setTimeout(ask, 200);
+      });
+  },
+  viewDepts: function () {
+    inquirer
+      .prompt({
+        type: "input",
+        name: "id",
+        message: "Enter an id to delete by, or nothing to delete everything.",
+      })
+      .then((res) => {
+        if (res.id == "") {
+          playlist.dropTable();
+          playlist.createTable();
+        } else {
+          playlist.deletePlaylists("id", res.id);
+        }
+        setTimeout(ask, 200);
+      });
+  },
+  viewRoles: function () {
+    inquirer
+      .prompt({
+        type: "input",
+        name: "id",
+        message: "Enter an id to delete by, or nothing to delete everything.",
+      })
+      .then((res) => {
+        if (res.id == "") {
+          playlist.dropTable();
+          playlist.createTable();
+        } else {
+          playlist.deletePlaylists("id", res.id);
+        }
+        setTimeout(ask, 200);
+      });
+  },
+  viewEmployees: function () {
+    inquirer
+      .prompt({
+        type: "input",
+        name: "id",
+        message: "Enter an id to delete by, or nothing to delete everything.",
+      })
+      .then((res) => {
+        if (res.id == "") {
+          playlist.dropTable();
+          playlist.createTable();
+        } else {
+          playlist.deletePlaylists("id", res.id);
+        }
+        setTimeout(ask, 200);
+      });
+  },
+  updateEmployeeRole: function () {
+    inquirer
+      .prompt({
+        type: "input",
+        name: "id",
+        message: "Enter an id to delete by, or nothing to delete everything.",
+      })
+      .then((res) => {
+        if (res.id == "") {
+          playlist.dropTable();
+          playlist.createTable();
+        } else {
+          playlist.deletePlaylists("id", res.id);
+        }
+        setTimeout(ask, 200);
+      });
+  },
+  deleteDept: function () {
+    inquirer
+      .prompt({
+        type: "input",
+        name: "id",
+        message: "Enter an id to delete by, or nothing to delete everything.",
+      })
+      .then((res) => {
+        if (res.id == "") {
+          playlist.dropTable();
+          playlist.createTable();
+        } else {
+          playlist.deletePlaylists("id", res.id);
+        }
+        setTimeout(ask, 200);
+      });
+  },
+  deleteRole: function () {
+    inquirer
+      .prompt({
+        type: "input",
+        name: "id",
+        message: "Enter an id to delete by, or nothing to delete everything.",
+      })
+      .then((res) => {
+        if (res.id == "") {
+          playlist.dropTable();
+          playlist.createTable();
+        } else {
+          playlist.deletePlaylists("id", res.id);
+        }
+        setTimeout(ask, 200);
+      });
+  },
+  deleteEmployee: function () {
+    inquirer
+      .prompt({
+        type: "input",
+        name: "id",
+        message: "Enter an id to delete by, or nothing to delete everything.",
+      })
+      .then((res) => {
+        if (res.id == "") {
+          playlist.dropTable();
+          playlist.createTable();
+        } else {
+          playlist.deletePlaylists("id", res.id);
+        }
+        setTimeout(ask, 200);
+      });
+  },
+  exit: function () {
+    process.exit();
+  },
+};
+
+function ask() {
+  inquirer.prompt(menu).then(function(choice) {
+    actions[choice.action]();
+  });
+}
 
 // MySQL Connection
 const connection = mysql.createConnection({
@@ -48,17 +266,15 @@ const connection = mysql.createConnection({
 
 connection.connect(function (err) {
   if (err) throw err;
-  console.log(chalk.cyan(`---------------------------------`));
-  console.log(chalk.cyan(`|                               |`));
-  console.log(chalk.cyan(`| ----- EMPLOYEE MANAGER ------ |`));
-  console.log(chalk.cyan(`|                               |`));
-  console.log(chalk.cyan(`---------------------------------`));
+  //   console.log(chalk.cyan(`---------------------------------`));
+  //   console.log(chalk.cyan(`|                               |`));
+  //   console.log(chalk.cyan(`| ----- EMPLOYEE MANAGER ------ |`));
+  //   console.log(chalk.cyan(`|                               |`));
+  //   console.log(chalk.cyan(`---------------------------------`));
   // Construct new classes
   departments = new departments.Departments(connection);
   roles = new roles.Roles(connection);
   employees = new employees.Employees(connection);
 
-  inquirer.prompt(menu);
-
-
+  ask();
 });
